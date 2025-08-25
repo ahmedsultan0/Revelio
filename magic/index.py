52367ca4d324205e1a4db26ced6799d4845d570f
Ahmed Sultan <ahmed.sultan.pub@gmail.com>
2025-08-25 14:52:46 +0300

    v1.0.2 - opps had a typo for the file name
    v1.0.2 - re-named imports

--- magic/index.py
+++ magic/index.py
@@ -1,9 +1,9 @@
 from magic.image_indexer import process_images
-import revilio
+import revelio
 from magic.console_utils import console
 
 def index():
     folder_to_index = console.input("Enter folder to index images: ").strip()
     images_data = process_images(folder_to_index)
-    revilio.global_index.clear()
-    revilio.global_index.update(images_data)
+    revelio.global_index.clear()
+    revelio.global_index.update(images_data)
