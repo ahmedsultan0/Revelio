baa55311514d06f6e9a8ab203cf077d4538c67b0
Ahmed Sultan <ahmed.sultan.pub@gmail.com>
2025-08-26 16:01:24 +0300

    v1.0.2 - supporting more image media types and ignoring known application's hidden directories

--- magic/config.py
+++ magic/config.py
@@ -1,14 +1,15 @@
 BL = "#192BC2"
 LB = "#57B2FF"
 
-IMAGE_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"}
+IMAGE_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".dib", ".tif", ".tiff", 
+                    ".webp", ".jpe", ".jfif", ".jfi", ".heic", ".heif", ".avif"}
 
 # OS Folders to exclude
 EXCLUDED_DIRS = [
-    "/System", "/Library", "/usr", "/bin", "/sbin", "/opt", "/Applications", # macOS/Linux
+    "/System", "/Library", "/usr", "/bin", "/sbin", "/opt", "/Applications", "/private/var" # macOS/Linux
     "C:\\Windows", "C:\\Program Files", "C:\\Program Files (x86)"  # Windows
 ]
 
 EXCLUDED_SUB_DIRS = [
-    "/.npm", "/.gradle", "/.npm", "/.vscode"
+    "/.npm", "/.gradle", "/.npm", "/.vscode", "/.rbenv", "/node_modules"
 ]
